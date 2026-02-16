f471e8d6bb6571251d91d7a87ad471a6bbccdb3e
Gowthamgowthu2003 <gowthamd901@gmail.com>
2026-02-16 21:24:45 +0530

    Initial commit - Full Stack BookMyStay Application

---
+++ backend/src/main/java/in/stay/utills/AppProperties.java
@@ -0,0 +1,8 @@
+package in.stay.utills;
+
+public class AppProperties 
+{
+   public static final String CLOUDINARY_CLOUD_NAME ="ddd5zaj3w";
+   public static final String CLOUDINARY_API_KEY="942441261888714";
+   public static final String CLOUDINARY_SECRET_KEY="xqF1YdLVaMnh-wCaTvgxYQXRTZE";
+}
